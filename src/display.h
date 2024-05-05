6b91c48bd288fa34e5a47b906204009efc126d70
Liam Mehle <liammehlex@gmail.com>
2024-05-05 17:16:44 +0200

    cleanup and refactor

--- src/display.h
+++ src/display.h
@@ -6,14 +6,6 @@ void setup_display();
 void display_toggle(bool toggle);
 void cleanTFT();
 
-/*
-void show_display(String header, int wait = 0);
-void show_display(String header, String line1, int wait = 0);
-void show_display(String header, String line1, String line2, int wait = 0);
-void show_display(String header, String line1, String line2, String line3, int wait = 0);
-void show_display(String header, String line1, String line2, String line3, String line4, int wait = 0);
-void show_display(String header, String line1, String line2, String line3, String line4, String line5, int wait = 0);
-*/
 void show_display(
     std::initializer_list<String> lines_of_text,
     int wait=0);
